bf1a7edfdc8bd89aa6c9b74f4b57ee9874f1593c
Hans Baker <Hans Baker>
2017-01-27 10:05:11 -0800

    adding ssl

--- bin/www/index.js
+++ bin/www/index.js
@@ -1,6 +1,15 @@
 var app = require("../../app.js");
+var https = require("https");
+var fs = require('fs');
+var config = require("../../config/config.json");
+
+var options = {
+    key: fs.readFileSync(sslPath + 'privkey.pem'),
+    cert: fs.readFileSync(sslPath + 'fullchain.pem')
+}
 
 app.listen(app.get("port"), function(){
     console.log("Server started on port " + app.get("port"));
 });
 
+https.createSever(options, app).listen(443);
